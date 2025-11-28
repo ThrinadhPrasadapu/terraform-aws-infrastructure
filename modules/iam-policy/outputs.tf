cc7290256d05920c2d6cb603743f57de85505574
Thrinadh Prasadapu <126080047+ThrinadhPrasadapu@users.noreply.github.com>
2025-11-28 22:38:25 +0530

    Delete modules directory

--- modules/iam-policy/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "iam_role" {
-  description = "iam role name"
-  value = aws_iam_role.iam_role.name
-}
--- modules/tag-policy/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "policy_id" {
-  value       = aws_organizations_policy.tag_policy.id
-  description = "ID of the tag policy."
-}
